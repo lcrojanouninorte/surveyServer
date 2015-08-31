186db97b449925f71c0dd17e774c29b29f038078
Luis Rojano <lcrojano@uninorte.edu.co>
2015-08-30 19:11:02 -0500

    cors header set on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::API
 	before_filter :set_access_control_headers
 
 def set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
+  headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 end
 end
