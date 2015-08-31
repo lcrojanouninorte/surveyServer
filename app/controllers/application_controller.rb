ee02be3eccfc08289ef49d7cb634f064cff54500
Luis Rojano <lcrojano@uninorte.edu.co>
2015-08-30 19:16:23 -0500

    cors header set on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,9 @@ class ApplicationController < ActionController::API
 
 	include ActionController::MimeResponds
 
-before_filter :cors_preflight_check
-after_filter :cors_set_access_control_headers
+	before_filter :set_access_control_headers
 
-def cors_set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-  headers['Access-Control-Max-Age'] = "1728000"
+def set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
 end
-
-def cors_preflight_check
-  if request.method == 'OPTIONS'
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-    headers['Access-Control-Max-Age'] = '1728000'
-
-    render :text => '', :content_type => 'text/plain'
-  end
 end
