fd2bc643ff27415927dc3adcdf11eeb3e3b3e6de
kevin agudelo <kevinagu90@gmail.com>
2015-09-09 19:06:39 -0500

    estilos a reportes excel

--- db/schema.rb
+++ db/schema.rb
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 20150908195334) do
     t.string   "p130"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "finished"
   end
 
   create_table "users", force: true do |t|
@@ -166,14 +167,8 @@ ActiveRecord::Schema.define(version: 20150908195334) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "auth_token"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "password_reset_token"
-    t.datetime "password_reset_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
